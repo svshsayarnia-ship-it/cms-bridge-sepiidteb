1a6809cbc0535a70e4ebca70f985104dcb274ab9
svshsayarnia-ship-it <svshsayarnia@gmail.com>
2026-08-02 10:12:00 +0330

    Delete app directory

--- app/components/JsonLd.tsx
+++
@@ -1,9 +0,0 @@
-export function JsonLd({ data }: { data: Record<string, unknown> }) {
-  return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-    />
-  );
-}
-
