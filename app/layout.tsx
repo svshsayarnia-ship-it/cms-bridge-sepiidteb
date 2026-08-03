dd7cf07355cd3e5c7ea6fbc56330db7507e0b1ff
svshsayarnia-ship-it <svshsayarnia@gmail.com>
2026-08-03 15:48:50 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" dir="rtl">
+    <html lang="fa" dir="ltr">
       <body>
         <SiteHeader />
         {children}
